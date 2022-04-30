1f9d83aecb8d59073fa672f72742049fffe9ef28
Learner <1181434433@qq.com>
2022-04-30 14:48:52 +0800

    "redisTemplate的使用"

--- dome_redisTemplate/src/test/java/com/dly/DomeRedisTemplateApplicationTests.java
+++ dome_redisTemplate/src/test/java/com/dly/DomeRedisTemplateApplicationTests.java
@@ -1,13 +1,32 @@
 package com.dly;
 
+import com.dly.pojo.User;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.io.Serializable;
 
 @SpringBootTest
 class DomeRedisTemplateApplicationTests {
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+        /**
+         * opsForValue()  --->  string
+         * opsForHash()  ---->   hash
+         * opsForList()  ----->   list
+         * opsForSet() ------>   set
+         * opsForZSet     ----->  Zset
+         * keys *
+         */
+        redisTemplate.opsForValue().set("user",new User("戴林宇",12));
+        Object user = redisTemplate.opsForValue().get("user");
+        System.out.println("user=" + user);
     }
-
 }
---
+++ dome_redisTemplate/src/test/java/com/dly/RedisStringTemp.java
@@ -0,0 +1,39 @@
+package com.dly;
+
+import com.dly.pojo.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@SpringBootTest
+public class RedisStringTemp {
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void  stringtest1(){
+        stringRedisTemplate.opsForValue().set("name","戴林宇");
+        String name = stringRedisTemplate.opsForValue().get("name");
+        System.out.println("name="+name);
+    }
+
+    private static final ObjectMapper mapper=new ObjectMapper();
+    @SneakyThrows
+    @Test
+    public void Objecttest1(){
+        //  主动处理
+        //手动序列化json放入到redis中
+        String userjson = mapper.writeValueAsString(new User("戴林宇", 12));
+        stringRedisTemplate.opsForValue().set("user",userjson);
+        //读取redis中存入的redis的json字符串 并反序列化成user对象
+        String user = stringRedisTemplate.opsForValue().get("user");
+        User user1 = mapper.readValue(user, User.class);
+        System.out.println("user="+user1);
+    }
+
+}
