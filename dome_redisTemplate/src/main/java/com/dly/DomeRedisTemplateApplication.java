31e2d18364da40512782b110d345d11f03c6c694
Learner <1181434433@qq.com>
2022-04-30 13:13:06 +0800

    "初始化"

---
+++ dome_redisTemplate/src/main/java/com/dly/DomeRedisTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.dly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomeRedisTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomeRedisTemplateApplication.class, args);
+    }
+
+}
