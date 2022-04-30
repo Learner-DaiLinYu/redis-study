557bb181c3ee6a25cf254b11a97f1fd737a679cb
Learner <1181434433@qq.com>
2022-04-30 16:05:13 +0800

    "初始化"

---
+++ hm-dianping/src/main/java/com/dly/HmDianpingApplication.java
@@ -0,0 +1,13 @@
+package com.dly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmDianpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmDianpingApplication.class, args);
+	}
+
+}
